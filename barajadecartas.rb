0aae9a6ab8601f27641b97c49c64b2d33e14da46
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-08 23:07:24 -0400

    Creación de métodos barajar y sacar

--- barajadecartas.rb
+++ barajadecartas.rb
@@ -11,4 +11,12 @@ class Baraja #Creación clase Baraja
             end
         end
     end
-    
+
+    def barajar #Creación método barajar que permite desordenar el arreglo de cartas
+        @cartas = @cartas.shuffle
+    end
+
+    def sacar #Creación método sacar que permite retirar la primera carta de la baraja 
+        @cartas.pop
+    end
+
