4beabf4602ff5fd15342f1b8f40d68aea47f47ad
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-08 20:38:10 -0400

    creación clase CuentaBancaria

---
+++ cuentabancaria.rb
@@ -0,0 +1,7 @@
+class CuentaBancaria #Creación clase Cuenta Bancaria
+    attr_accessor :banco, :numero_cuenta, :saldo
+    def initialize(banco, numero_cuenta, saldo = 0)
+        @banco = banco
+        @numero_cuenta = numero_cuenta
+        @saldo = saldo
+    end
