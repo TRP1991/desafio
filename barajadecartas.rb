c350f8c208e10c6b4c74b1347d2a9402966db656
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-08 23:04:23 -0400

    Se crea clase Baraja

--- barajadecartas.rb
+++ barajadecartas.rb
@@ -1,2 +1,14 @@
 require_relative 'Carta'
 
+class Baraja #Creación clase Baraja
+    attr_accessor :cartas
+    def initialize 
+        pintas = ['C', 'C', 'E', 'T']
+        @cartas = []
+        pintas.each do |p|
+            13.times do |i|
+                @cartas.push(Carta.new(i + 1, p))
+            end
+        end
+    end
+    
