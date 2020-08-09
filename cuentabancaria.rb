adecde7111eac0bf96c7cba578bf109bf85fadad
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-08 20:53:40 -0400

    se actualiza método saldo por saldo_total, se realiza la transferencia de saldos totales entre cuentas

--- cuentabancaria.rb
+++ cuentabancaria.rb
@@ -19,7 +19,7 @@ class Usuario #Creación clase Usuario
         @cuentas_bancarias = cuentas_bancarias
     end
 
-    def saldo #Creación método saldo 
+    def saldo_total #Creación método saldo_total
         total = 0
         @cuentas_bancarias.count.times do |i|
             total += @cuentas_bancarias[i].saldo
@@ -28,3 +28,17 @@ class Usuario #Creación clase Usuario
     end
 end
 
+require_relative 'Usuario'
+require_relative 'Cuenta Bancaria'
+
+cuenta1 = CuentaBancaria.new("Banco de Chile", "018088356", 5000)
+cuenta2 = CuentaBancaria.new("Banco Santander", "015328860", 5000)
+
+cuenta1.transferir(5000, cuenta2)
+
+puts cuenta1.saldo
+puts cuenta2.saldo
+
+usuario = Usuario.new("Tamara Rubilar Pinilla", [cuenta1, cuenta2])
+
+puts usuario.saldo_total
