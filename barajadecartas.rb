aac2770e477b72fad314ea672319a728d627a0e6
TRP1991 <57776064+TRP1991@users.noreply.github.com>
2020-08-10 19:50:33 -0400

    Corregido, método shuffle y método repartir_mano
    Corregido, método shuffle y método repartir_mano

--- barajadecartas.rb
+++ barajadecartas.rb
@@ -1,6 +1,7 @@
 require_relative 'Carta'
 
 class Baraja #Creación clase Baraja
+    attr_accessor :cartas 
 
     def initialize
         @cartas = []
@@ -11,8 +12,8 @@ class Baraja #Creación clase Baraja
         end
     end
 
-    def barajar #Creación método barajar que permite desordenar el arreglo de cartas
-        @cartas = @cartas.shuffle
+    def shuffle #Creación método shuffle que permite desordenar el arreglo de cartas
+        @cartas.shuffle!
     end
 
     def sacar #Creación método sacar que permite retirar la primera carta de la baraja 
@@ -21,7 +22,7 @@ class Baraja #Creación clase Baraja
 
     def repartir_mano  #Creación método repartir_mano que permite devolver un arreglo con las primeras 5 cartas de la baraja
         mano = []
-        5.times {mano << pop }
+        5.times {mano.push(sacar)}
         return mano
     end
 end
